3a5b69dece06edb8f7c2cc066ab9f88e7192e51d
Toheedullah-K3 <toheedullah002@gmail.com>
2025-12-08 17:23:09 +0500

    Categories added

---
+++ client/src/components/ProductCard.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { ProductTypes } from "@/types"
+
+
+function ProductCard({product}: {product: ProductTypes}) {
+  return (
+    <div>
+      ProductCard
+    </div>
+  )
+}
+
+export default ProductCard
