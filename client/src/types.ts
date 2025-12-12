d5d29bb83b6e6f7ce0d5759f9b2f9e5ba35b153b
Toheedullah-K3 <toheedullah002@gmail.com>
2025-12-12 12:27:57 +0500

    Zod schemas added for Shipping Form

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,3 +1,5 @@
+import { email, z } from "zod";
+
 export type ProductType = {
     id: string | number;
     name: string;
@@ -18,3 +20,18 @@ export type CartItemsType = ProductType & {
 }
 
 export type CartItemsTypes = CartItemsType[]
+
+// zod schema
+export const shippingFormSchema = z.object({
+    name: z.string().trim().nonempty("Name is Required!"),
+    email: z.email().trim().nonempty("Email is Required!"),
+    phone: z
+        .string()
+        .min(7, "Phone number must be between 7 to 10 digits")
+        .max(10, "Phone number must be between 7 to 10 digits")
+        .regex(/^\d+$/, "Phone number must only contain number"),
+    address: z.string().trim().nonempty("Address is Required!"),
+    city: z.string().trim().nonempty("City is Required!")
+})
+
+export type shippingFormInputs = z.infer<typeof shippingFormSchema>
